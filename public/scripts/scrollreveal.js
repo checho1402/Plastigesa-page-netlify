39dda980dcce9c07dd4ec15733c7018cf27ab85d
checho1402 <sergio.ramos.villena@ucsp.edu.pe>
2024-11-05 13:24:20 -0500

    commit actualizacion2

---
+++ public/scripts/scrollreveal.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = [
+        { selector: '.top-element', options: { origin: 'top' } },
+        { selector: '.bottom-element', options: { origin: 'bottom' } },
+        { selector: '.left-element', options: { origin: 'left' } },
+        { selector: '.right-element', options: { origin: 'right' } },
+    ];
+
+    elements.forEach((el) => {
+        ScrollReveal().reveal(el.selector, {
+            duration: 1000,
+            distance: '50px',
+            easing: 'ease-in-out',
+            reset: false,
+            interval: 200,
+            ...el.options
+        });
+    });
+});
